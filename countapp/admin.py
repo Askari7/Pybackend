d71298fe93359400f76358103b2350163943beab
askari7 <askariabbaas135@gmail.com>
2024-11-03 19:52:07 +0500

    countapp back

--- countapp/admin.py
+++ countapp/admin.py
@@ -5,7 +5,7 @@ from .models import Record  # Import the Record model
 # Register the Record model with the admin site
 @admin.register(Record)
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('in_count', 'out_count', 'timestamp')  # Customize the list display
+    list_display = ('in_count', 'out_count', 'timestamp','total_count')  # Customize the list display
     search_fields = ('in_count', 'out_count')  # Add search functionality
     list_filter = ('timestamp',)  # Add filtering by timestamp
 
--- countapp/models.py
+++ countapp/models.py
@@ -2,9 +2,10 @@
 from django.db import models
 
 class Record(models.Model):
+    total_count = models.IntegerField()  # Field for "In" value
     in_count = models.IntegerField()  # Field for "In" value
     out_count = models.IntegerField()  # Field for "Out" value
     timestamp = models.DateTimeField()  # Field for "Time" value
 
     def __str__(self):
-        return f"Record(In={self.in_count}, Out={self.out_count}, Time={self.timestamp})"
+        return f"Record(In={self.in_count}, Out={self.out_count}, Time={self.timestamp}, Total={self.total_count})"
